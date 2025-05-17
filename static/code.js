e7a1a814d8820e0d1e411fe06ba6d6ee68b81feb
marius david <marius@mariusdavid.fr>
2025-05-17 15:00:11 +0200

    initial popup

--- static/code.js
+++ static/code.js
@@ -1,3 +1,13 @@
+// Why isn’t that part of Javascript... I’m not surprised CSP exist with that.
+function escapeHtml(unsafe) {
+    return unsafe
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;");
+}
+
 function updateStatus(content) {
     document.getElementById("status").textContent = "status: " + content
 }
@@ -29,8 +39,9 @@ xhr.onreadystatechange = function() {
             saved_answer = JSON.parse(this.responseText);
 
             // nature which are typically not associated with coordinates
+            updateStatus("processing...");
             refresh_map();
-            updateStatus("ready");
+            updateStatus("ready to use!");
         } else {
             alert("No valid response received");
             updateStatus("failure");
@@ -60,7 +71,48 @@ function refresh_map() {
             continue;
         }
 
-        let marker = L.marker(entry["pos"], {});
+        let name = entry["name"];
+        if (name == null) {
+            name = "no/unknown name";
+        }
+
+
+
+        //TODO: generate HTML on-the-fly (for faster processing time)
+
+        popupHTML = "<b>" + escapeHtml(name) + "</b><br />";
+
+        //TODO: determine nature in the server-side code to display it here like in the original
+        location_name = entry["location_name"]
+        if (location_name != null) {
+            popupHTML += "<i>" + escapeHtml(location_name) + "</i>";
+        }
+
+        // image
+        //TODO:
+        /*imageURL = null
+        imageCredit = null
+        if (qid in extra_images) {
+            imageURL = "./extra/" + qid + "." + extra_images[qid][0];
+            imageCredit = "image from <a href=\"" + extra_images[qid][1] + "\">here</a>"
+        } else if (entry["image"] != undefined) {
+            imageURL = escapeHtml(entry["image"]["value"]);
+            imageCredit = "image from Wikimedia commons";
+        }
+        if (imageURL != null) {
+            popupHTML += "<img src=\"" + imageURL + "\" class=\"embed-image\"/><br /><p>" + imageCredit + "</p><br />";
+        } else {
+            popupHTML += "<p><i>No image</i></p><br />"
+            console.log(entry["itemLabel"]["value"] + " ( https://wikidata.org/wiki/" + qid + " ) have no image")
+        }*/
+
+        // link
+        popupHTML += "<a href=\"" + escapeHtml(entry["source_url"]) + "\">Data source</a>"
+
+        let marker = L.marker(entry["pos"], {})
+            .bindPopup(popupHTML, {
+                maxWidth: "auto",
+            });
 
         markers.addLayers(marker);
     }
