6e4ba7fbb3ecd4d054a830df4d846c5ef691f071
marius david <marius@mariusdavid.fr>
2025-05-24 20:30:42 +0200

    format JS and HTML

--- static/code.js
+++ static/code.js
@@ -1,35 +1,36 @@
 // Why isn’t that part of Javascript... I’m not surprised CSP exist with that.
 function escapeHtml(unsafe) {
-    return unsafe
-         .replace(/&/g, "&amp;")
-         .replace(/</g, "&lt;")
-         .replace(/>/g, "&gt;")
-         .replace(/"/g, "&quot;")
-         .replace(/'/g, "&#039;");
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
 }
 
 function updateStatus(content) {
-    document.getElementById("status").textContent = "status: " + content
+  document.getElementById("status").textContent = "status: " + content;
 }
 
 updateStatus("Loading web page");
 
-let map = L.map('map', {
-    fullscreenControl: true,
-    fullscreenControlOptions: {
-        position: 'topleft'
-    }
+let map = L.map("map", {
+  fullscreenControl: true,
+  fullscreenControlOptions: {
+    position: "topleft",
+  },
 }).setView([12.9, 15.3], 3);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-var sidebar = L.control.sidebar('sidebar', {
-    position: 'right',
-    closeButton: false, //TODO: would be nice to close (and re-open) this
-    autoPan: false,
+var sidebar = L.control.sidebar("sidebar", {
+  position: "right",
+  closeButton: false, //TODO: would be nice to close (and re-open) this
+  autoPan: false,
 });
 
 map.addControl(sidebar);
@@ -40,122 +41,126 @@ let saved_answer = null;
 let existing_layer = null;
 
 function refresh_map_if_needed() {
-    if (existing_layer != null) {
-        refresh_map();
-    }
+  if (existing_layer != null) {
+    refresh_map();
+  }
 }
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4) {
-        if (this.status == 200) {
-            saved_answer = JSON.parse(this.responseText);
+xhr.onreadystatechange = function () {
+  if (this.readyState == 4) {
+    if (this.status == 200) {
+      saved_answer = JSON.parse(this.responseText);
 
-            // nature which are typically not associated with coordinates
-            updateStatus("processing...");
-            refresh_map();
-            updateStatus("ready to use!");
-        } else {
-            alert("No valid response received");
-            updateStatus("failure");
-        }
+      // nature which are typically not associated with coordinates
+      updateStatus("processing...");
+      refresh_map();
+      updateStatus("ready to use!");
+    } else {
+      alert("No valid response received");
+      updateStatus("failure");
     }
-}
+  }
+};
 
 function refresh_map() {
-    console.log("refresh_map called");
+  console.log("refresh_map called");
 
-    if (existing_layer != null) {
-        map.removeLayer(existing_layer);
+  if (existing_layer != null) {
+    map.removeLayer(existing_layer);
+  }
+
+  const ignore_exhibits = !document.getElementById("include_exhibits").checked;
+  const missing_image_only =
+    document.getElementById("missing_image_only").checked;
+
+  let markers = L.markerClusterGroup({
+    maxClusterRadius: 30,
+  });
+
+  existing_layer = markers;
+
+  for (entry of Object.entries(saved_answer)) {
+    entry = entry[1];
+    //console.log(entry);
+
+    if (entry["pos"] == null) {
+      continue;
     }
 
-    const ignore_exhibits = !document.getElementById("include_exhibits").checked;
-    const missing_image_only = document.getElementById("missing_image_only").checked;
+    if (ignore_exhibits && entry["is_in_exhibit"] == true) {
+      continue;
+    }
 
-    let markers = L.markerClusterGroup({
-        "maxClusterRadius": 30,
+    if (missing_image_only && entry["image"] != null) {
+      continue;
+    }
+
+    let name = entry["name"];
+    if (name == null) {
+      name = "no/unknown name";
+    }
+
+    //TODO: generate HTML on-the-fly (for faster processing time)
+
+    popupHTML = "<b>" + escapeHtml(name) + "</b><br />";
+
+    location_name = entry["location_name"];
+    nature_name = entry["nature"];
+    if (location_name != null || nature_name != null) {
+      popupHTML += "<i>";
+      if (nature_name != null) {
+        popupHTML += escapeHtml(nature_name);
+        if (location_name != null) {
+          popupHTML += " — ";
+        }
+      }
+      if (location_name != null) {
+        popupHTML += escapeHtml(location_name);
+      }
+      popupHTML += "</i><br />";
+    }
+
+    // image
+    imageURL = entry["image"]; //TODO: (more server-side) some images are TIF that doesn’t display in browser
+    imageSourceURL = entry["image_source_url"];
+
+    if (imageURL != null) {
+      popupHTML +=
+        '<img src="' + escapeHtml(imageURL) + '" class="embed-image"/><br />';
+      imageSourceText = entry["image_source_text"];
+
+      if (imageSourceURL != null) {
+        if (imageSourceText == null) {
+          imageSourceText = "image source";
+        }
+        popupHTML +=
+          '<p><a href="' +
+          escapeHtml(imageSourceURL) +
+          '">' +
+          escapeHtml(imageSourceText) +
+          "</a></p>";
+      } else {
+        if (imageSourceText == null) {
+          imageSourceText = "image source somehow unknwown.";
+        }
+        popupHTML += "<p>" + escapeHtml(imageSourceText) + "</p>";
+      }
+    } else {
+      popupHTML += "<p><i>No image</i></p><br />";
+    }
+
+    // link
+    popupHTML +=
+      '<a href="' + escapeHtml(entry["source_url"]) + '">Data source</a>';
+
+    let marker = L.marker(entry["pos"], {}).bindPopup(popupHTML, {
+      maxWidth: "auto",
     });
 
-    existing_layer = markers;
+    markers.addLayers(marker);
+  }
 
-    for (entry of Object.entries(saved_answer)) {
-
-        entry = entry[1];
-        //console.log(entry);
-        
-        if (entry["pos"] == null) {
-            continue;
-        }
-
-        if (ignore_exhibits && entry["is_in_exhibit"] == true) {
-            continue;
-        }
-
-        if (missing_image_only && entry["image"] != null) {
-            continue;
-        }
-
-        let name = entry["name"];
-        if (name == null) {
-            name = "no/unknown name";
-        }
-
-
-
-        //TODO: generate HTML on-the-fly (for faster processing time)
-
-        popupHTML = "<b>" + escapeHtml(name) + "</b><br />";
-
-        location_name = entry["location_name"]
-        nature_name = entry["nature"]
-        if (location_name != null || nature_name != null) {
-            popupHTML += "<i>";
-            if (nature_name != null) {
-                popupHTML += escapeHtml(nature_name);
-                if (location_name != null) {
-                    popupHTML += " — ";
-                }
-            }
-            if (location_name != null) {
-                popupHTML += escapeHtml(location_name);
-            }
-            popupHTML += "</i><br />"
-        }
-
-        // image
-        imageURL = entry["image"] //TODO: (more server-side) some images are TIF that doesn’t display in browser
-        imageSourceURL = entry["image_source_url"]
-
-        if (imageURL != null) {
-            popupHTML += "<img src=\"" + escapeHtml(imageURL) + "\" class=\"embed-image\"/><br />";
-            imageSourceText = entry["image_source_text"]
-            
-            if (imageSourceURL != null) {
-              if (imageSourceText == null) {
-                imageSourceText = "image source"
-              }
-              popupHTML += "<p><a href=\"" + escapeHtml(imageSourceURL) + "\">" + escapeHtml(imageSourceText) + "</a></p>";
-            } else {
-              if (imageSourceText == null) {
-                imageSourceText = "image source somehow unknwown."
-              }
-              popupHTML += "<p>" + escapeHtml(imageSourceText) + "</p>";
-            }
-        } else {
-            popupHTML += "<p><i>No image</i></p><br />"
-        }
-
-        // link
-        popupHTML += "<a href=\"" + escapeHtml(entry["source_url"]) + "\">Data source</a>"
-
-        let marker = L.marker(entry["pos"], {})
-            .bindPopup(popupHTML, {
-                maxWidth: "auto",
-            });
-
-        markers.addLayers(marker);
-    }
-
-    map.addLayer(markers)
+  map.addLayer(markers);
 }
 
 updateStatus("fetching datas");
